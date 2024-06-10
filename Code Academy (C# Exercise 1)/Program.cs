b586e6c9125c71123833e166709fa70dfc1778f9
baricuatro_angelo <angelolouis.baricuatro.20@usjr.edu.ph>
2024-06-10 11:31:42 +0800

    C# Exercise
    Basic Conversion of Data types

---
+++ Code Academy (C# Exercise 1)/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace firstExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool convertableBool = true;
+            string strToChar = "Meme";
+            int intToDouble = 34;
+            double doubleToInt = 21.33;
+
+            // Convert bool to string
+            Console.WriteLine(convertableBool);
+            Console.WriteLine(convertableBool.ToString());
+            // Convert string to char
+            Console.WriteLine(strToChar.ToCharArray());
+            // Convert int to double -> Implicit Conversion
+            double convertedInt = intToDouble;
+            Console.WriteLine(convertedInt);
+            // Convert double to int -> Explicit Conversion
+            int convertedDouble = (int)doubleToInt;
+            Console.WriteLine(convertedDouble);
+        }
+    }
+}
