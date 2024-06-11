64c76db279fce5e08f9d1fb1d99f689e4ca64086
baricuatro_angelo <angelolouis.baricuatro.20@usjr.edu.ph>
2024-06-11 10:04:59 +0800

    Numerical Exercise v1.4
    Use built-in methods to find lowest number

---
+++ Built-In Methods (Numerical Exercise)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LowestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Starting variables 
+            int numberOne = 12932;
+            int numberTwo = -2828472;
+
+            // Use built-in methods and save to variable 
+            double numberOneSqrt = Math.Floor(Math.Sqrt(numberOne));
+            // Use built-in methods and save to variable
+            // Math.Floor converts decimal to nearest whole number
+            // Math.Sqrt finds the Square Root of the whole number
+            /* Since numberTwo is a negative, use Math.Abs to find absolute 
+               value to make the number positive */
+            double numberTwoSqrt = Math.Floor(Math.Sqrt(Math.Abs(numberTwo)));
+
+            // Print the lowest number
+            // Math.Min returns the smaller of two numbers
+            Console.WriteLine(Math.Min(numberOneSqrt, numberTwoSqrt));
+
+        }
+    }
+}
