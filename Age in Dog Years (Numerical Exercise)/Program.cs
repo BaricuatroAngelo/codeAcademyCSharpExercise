0bb1189679e91d8f47d05c6f88dde5c954f3a196
baricuatro_angelo <angelolouis.baricuatro.20@usjr.edu.ph>
2024-06-11 10:44:36 +0800

    Numerical Exercise v1.7
    Created a code that will calculate human years to dog years

---
+++ Age in Dog Years (Numerical Exercise)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* use this space to write your own short program! 
+            Here's what you learned:
+
+            DATA TYPES: int, double
+            ARITHMETIC OPERATORS: +, -, *, /
+            INCREMENT/DECREMENT: ++, --
+            MODULO: % 
+            BUILT-IN METHODS: Abs, Pow, Sqrt, Floor, Ceiling, Min, Max
+
+            Good luck! */
+
+            // Create a code that will calculate your age to dog years
+            int dogYears = 7;
+
+            Console.WriteLine("Please enter your age ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Your age in dog years is {age * dogYears}");
+            
+
+        }
+    }
+}
