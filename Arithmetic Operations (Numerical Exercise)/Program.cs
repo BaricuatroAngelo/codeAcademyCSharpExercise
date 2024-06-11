8aa5f7b128527fb3f701dab49867d8d94e55d2c2
baricuatro_angelo <angelolouis.baricuatro.20@usjr.edu.ph>
2024-06-11 09:51:20 +0800

    Numerical Exercise v1.1
    Planet Age Calculation Exercise

---
+++ Arithmetic Operations (Numerical Exercise)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PlanetCalculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Your Age
+            int userAge = 23;
+
+            // Length of years on Jupiter (in Earth years)
+            double jupiterYears = 11.86;
+
+            // Age on Jupiter
+            double jupiterAge = userAge / jupiterYears;
+
+            // Time to Jupiter
+            double journeyToJupiter = 6.142466;
+
+            // New Age on Earth
+            double newEarthAge = userAge + journeyToJupiter;
+
+            // New Age on Jupiter
+            double newJupiterAge = newEarthAge / jupiterYears;
+
+            // Log calculations to console
+            // Logs age on Earth
+            Console.WriteLine(userAge);
+            // Logs age on Jupiter
+            Console.WriteLine(jupiterAge);
+            // Logs age after journey to jupiter
+            Console.WriteLine(newEarthAge);
+            // Logs jupiter age after journey to jupiter
+            Console.WriteLine(newJupiterAge);
+        }
+    }
+}
